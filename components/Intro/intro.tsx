1a239499027c8d1d80385a4b26b9af9058aabe60
heinbl <9540040+heinbl@users.noreply.github.com>
2023-03-22 17:51:41 +0100

    intro changes. create 2nd paragraph.

--- components/Intro/intro.tsx
+++ components/Intro/intro.tsx
@@ -14,59 +14,69 @@ export interface IntroProps {};
 export default function Intro(props: IntroProps): ReactElement {
   return (
     <div className={appStyles.container}>
-      <div className={introStyles.topContainer}>
-        <section className={introStyles.infoBox}>
-          <div className={introStyles.hero}>
-            <h1 className={[introStyles.headline].join(" ")}>
-              Moin.<br />
-              Ich bin Simon,
-            </h1>
-            <h2 className={introStyles.subHeadline}>
-              ein Full-Stack-Entwickler aus dem schönen Oldenburg.
-            </h2>
+      <div id="intro" className={appStyles.pageHeight}>
+        <div className={introStyles.topContainer}>
+          <section className={introStyles.infoBox}>
+            <div className={introStyles.hero}>
+              <h1 className={[introStyles.headline].join(" ")}>
+                Moin.<br />
+                Ich bin Simon,
+              </h1>
+              <h2 className={introStyles.subHeadline}>
+                ein Full-Stack-Entwickler aus dem schönen Oldenburg.
+              </h2>
 
-            <h2 className={introStyles.subHeadline}>
-              Außerdem liebe ich Basketball, {' '}
-              arbeite gerne mit
-              {' '}
-              <Link href="/drechseln">
-                Holz
-              </Link>
-              {' '}
-              und fahre
-              {' '}
-              Rennrad.
-    { /* 
+              <h2 className={introStyles.subHeadline}>
+                Außerdem liebe ich Basketball, {' '}
+                arbeite gerne mit
+                {' '}
+                <Link href="/drechseln">
+                  Holz
+                </Link>
+                {' '}
+                und fahre
+                {' '}
+                Rennrad.
+                { /* 
               <Link href="/fahrrad" >Rennrad</Link>.
-  */}
-            </h2>
-            <div className={ [appStyles.inlineContainer, appStyles.flexV].join(" ") } >
-              <a
-                type="button"
-                className={[appStyles.actionButton, appStyles.linkNoStyles].join(" ")} href="#"
-              >
-                Schreib &apos;ne Mail
-                <ArrowRight size="1rem" />
-              </a>
-              <a className={[appStyles.actionButton, appStyles.linkNoStyles].join(" ")} target="_blank" rel="noopener noreferrer" href="mailto:simon.meyer@mailbox.org">
-                Portfolio
-                <ArrowDown size="1rem" />
-              </a>
-    { /* 
+                   */}
+              </h2>
+              <div className={ [appStyles.inlineContainer, appStyles.flexV].join(" ") } >
+                <button className={ appStyles.actionButton }>
+                  <a 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    href="mailto:simon.meyer@mailbox.org">
+                    Schreib &apos;ne Mail
+                  </a>
+                    <ArrowRight size="1rem" />
+                </button>
+                  <a
+                    className={ appStyles.actionButton }
+                    href="#portfolio"
+                  >
+                    Portfolio
+                    <ArrowDown size="1rem" />
+                  </a>
+                { /* 
               <button
                 type="button"
                 className={appStyles.actionButton}
               >
               </button>
-  */ }
+                   */ }
+              </div>
             </div>
-          </div>
-    { /* 
+            { /* 
           <a className={introStyles.mailMeButton} target="_blank" rel="noopener noreferrer" href="mailto:simon.meyer@mailbox.org">
             Schreib &apos;ne Mail
           </a>
-  */}
-        </section>
+               */}
+          </section>
+        </div>
+      </div>
+      <div id="portfolio" className={ [appStyles.pageHeight].join(" ") }>
+        stuff
       </div>
     </div>
   );
