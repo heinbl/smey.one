7675d7fbbf45596009e864d0da08c200dab7857e
Simon M <9540040+smeyx@users.noreply.github.com>
2023-04-20 16:38:23 +0200

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@styles/globals.css'
 import Head from 'next/head';
 import Layout from '@components/layout';
-import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
